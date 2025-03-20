6da1fac177c2cd0de7f46cc3b9cb6bc5a6824ba6
SofiaNords <sofia.k.nordstrom@gmail.com>
2025-03-20 14:16:06 +0100

    Create CustomerService & GetCustomersAsync

---
+++ Services/CustomerService.cs
@@ -0,0 +1,19 @@
+﻿using EStoreAPI.Models;
+
+namespace EStore.Services
+{
+    public class CustomerService
+    {
+        private readonly HttpClient _httpClient;
+
+        public CustomerService(HttpClient httpClient)
+        {
+            _httpClient = httpClient;
+        }
+
+        public async Task<List<CustomerDto>> GetCustomersAsync()
+        {
+            return await _httpClient.GetFromJsonAsync<List<CustomerDto>>("api/products");
+        }
+    }
+}
