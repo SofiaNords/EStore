4d9e3b21f445e9a2d216b35c078b5539ffd76cd6
SofiaNords <sofia.k.nordstrom@gmail.com>
2025-03-20 14:48:24 +0100

    Create Customers page - Add GetCustomersAsync - Add Customer table

---
+++ Components/Pages/Customers.razor.cs
@@ -0,0 +1,35 @@
+﻿using EStore.Services;
+using EStoreAPI.Entities;
+using EStoreAPI.Models;
+using Microsoft.AspNetCore.Components;
+
+namespace EStore.Components.Pages
+{
+    public partial class Customers
+    {
+        private bool _isLoading = true;
+
+        private string _errorMessage;
+
+        private List<CustomerDto> _customers = new List<CustomerDto>();
+
+        [Inject]
+        public CustomerService CustomerService { get; set; }
+
+        protected override async Task OnInitializedAsync()
+        {
+            try
+            {
+                _customers = await CustomerService.GetCustomersAsync();
+            }
+            catch (Exception ex)
+            {
+                _errorMessage = $"Något gick fel: {ex.Message}";
+            }
+            finally
+            {
+                _isLoading = false;
+            }
+        }
+    }
+}
