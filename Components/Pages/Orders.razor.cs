f0cc36cf1674748820a318f05e4f62ba4b1eda53
SofiaNords <sofia.k.nordstrom@gmail.com>
2025-03-21 08:57:56 +0100

    Create OnInitializesAsync in Orders.razor.cs

--- Components/Pages/Orders.razor.cs
+++ Components/Pages/Orders.razor.cs
@@ -1,6 +1,35 @@
-﻿namespace EStore.Components.Pages
+﻿using EStore.Services;
+using EStoreAPI.Models;
+using Microsoft.AspNetCore.Components;
+
+namespace EStore.Components.Pages
 {
     public partial class Orders
     {
+        private bool _isLoading = true;
+
+        private List<OrderDto> _orders = new List<OrderDto>();
+
+        private string _errorMessage;
+
+        [Inject]
+        public OrderService OrderService { get; set; }
+
+        protected override async Task OnInitializedAsync()
+        {
+            try
+            {
+                _orders = await OrderService.GetOrdersAsync();
+            }
+            catch (Exception ex)
+            {
+                _errorMessage = $"Något gick fel: {ex.Message}";
+            }
+            finally
+            {
+                _isLoading = false;
+            }
+        }
+
     }
 }
