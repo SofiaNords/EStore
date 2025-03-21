404d31eda9545d06fe56f265faf830601b9b6095
SofiaNords <sofia.k.nordstrom@gmail.com>
2025-03-21 08:50:18 +0100

    Create OrderService & inject HttpClient

---
+++ Services/OrderService.cs
@@ -0,0 +1,14 @@
+﻿namespace EStore.Services
+{
+    public class OrderService
+    {
+        private readonly HttpClient _httpClient;
+
+
+        public OrderService(HttpClient httpClient)
+        {
+            _httpClient = httpClient;
+        }
+
+    }
+}
