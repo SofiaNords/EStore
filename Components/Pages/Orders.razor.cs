7638de78d264ef71ac19b56eeaa58ea4b737a8b6
SofiaNords <sofia.k.nordstrom@gmail.com>
2025-03-21 13:15:13 +0100

    Add create methods i Orders.razor.cs -PrepareCreate -HandleCreateSave -HandleCreateCancel

--- Components/Pages/Orders.razor.cs
+++ Components/Pages/Orders.razor.cs
@@ -1,15 +1,17 @@
 ﻿using EStore.Services;
 using EStoreAPI.Models;
 using Microsoft.AspNetCore.Components;
+using System.Linq.Expressions;
 
 namespace EStore.Components.Pages
 {
     public partial class Orders : ComponentBase
     {
         private bool _isLoading = true;
+        private bool _isCreationModalVisible = false;
 
         private List<OrderDto> _orders = new List<OrderDto>();
-
+        private OrderForCreationDto _orderForCreation = new OrderForCreationDto();
         private string _errorMessage;
 
         [Inject]
@@ -31,5 +33,31 @@ namespace EStore.Components.Pages
             }
         }
 
+        private void PrepareCreate()
+        {
+            _orderForCreation = new OrderForCreationDto();
+            _isCreationModalVisible = true;
+        }
+
+        private async Task HandleCreateSave(OrderForCreationDto newOrder)
+        {
+            try
+            {
+                var createdOrder = await OrderService.CreateOrderAsync(newOrder);
+
+                _orders.Add(createdOrder);
+
+                _isCreationModalVisible = false;
+            }
+            catch (Exception ex)
+            {
+                _errorMessage = $"Något gick fel: {ex.Message}";
+            }
+        }
+
+        private void HandleCreateCancel()
+        {
+            _isCreationModalVisible = false;
+        }
     }
 }
